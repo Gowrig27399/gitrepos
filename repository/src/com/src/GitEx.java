f3fed3ba3070c635b2e403bd856ebe74fda439ff
Gowrig27399 <gowrig27399@gmail.com>
2021-02-08 20:39:45 +0530

    first commit

---
+++ repository/src/com/src/GitEx.java
@@ -0,0 +1,15 @@
+package com.src;
+
+public class GitEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+
+		System.out.println("adding 2 number");
+		int a=5,b=7;
+		int c=a+b;
+		System.out.println("c:"+c);
+	}
+
+}
